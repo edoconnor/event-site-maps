97ea8e3cdfc48f5f5c8c3cf664947166fcd2019a
Ed O'Connor <ed@oconnor.app>
2023-04-01 15:11:52 -0400

    map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,96 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LocationService } from '../services/location.service';
+import { map } from 'rxjs/operators';
+import 'leaflet/dist/leaflet.css';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  map: L.Map | null = null;
+  marker: L.Marker | null = null;
+  routePoints: L.LatLng[] = [];
 
-  ngOnInit(): void {
+  animatedCircleIcon = {
+    icon: L.divIcon({
+      className: 'gps_marker_icon',
+      html: '<div class="gps_marker"></div>',
+      iconSize: [20, 20],
+    }),
+  };
+
+  constructor(private locationService: LocationService) {}
+
+  ngOnInit() {
+    const fixedLocation = L.latLng(0, 0);
+    this.initMap(fixedLocation);
+
+    this.getPosition().subscribe(
+      (position) => {
+        if (this.map && this.marker) {
+          this.marker.setLatLng([position.latitude, position.longitude]);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
+  initMap(fixedLocation: L.LatLng) {
+    this.map = L.map('map').setView(fixedLocation, 16);
+    L.tileLayer(
+      'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png',
+      {
+        minZoom: 14,
+        maxNativeZoom: 20,
+        maxZoom: 20,
+        crossOrigin: true,
+      }
+    ).addTo(this.map);
+
+    this.marker = L.marker([0, 0], this.animatedCircleIcon).addTo(this.map);
+
+    this.getPosition().subscribe(
+      (position) => {
+        const latLng = L.latLng(position.latitude, position.longitude);
+        if (this.map && this.marker) {
+          this.marker.setLatLng(latLng);
+          this.map.setView(latLng);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  getPosition(): Observable<{ latitude: number; longitude: number }> {
+    return new Observable<{ latitude: number; longitude: number }>(
+      (observer) => {
+        if (navigator.geolocation) {
+          const watchId = navigator.geolocation.watchPosition(
+            (position) => {
+              const latitude = position.coords.latitude;
+              const longitude = position.coords.longitude;
+              this.locationService.setPosition({ latitude, longitude });
+              if (this.map && this.marker) {
+                this.marker.setLatLng([latitude, longitude]);
+              }
+              observer.next({ latitude, longitude });
+            },
+            (error) => {
+              observer.error(error);
+            }
+          );
+        } else {
+          observer.error('Geolocation is not supported by this browser.');
+        }
+      }
+    );
+  }
 }
